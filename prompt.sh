ef18fc2972a2b16467d574444ab84a4265792868
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-11-05 20:16:53 -0400

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -25,11 +25,11 @@ REL="$(lsb_release -d)"
 UP="$(uptime -p)"
 
 echo "
-  \    /\   $(whoami)@$(hostname) on ${REL:13}
-   )  ( ')  CPU: $(mpstat | awk 'END{print 100-$NF"%"}')
-  (  /  )   MEM: $(awk '/^Mem/ {print $3}' <(free -m))mb/$(awk '/^Mem/ {print $2}' <(free -m))mb
-   \(__)|   DSK: $(awk '/^\/dev/ {print $3}' <(df -H | grep "/dev/sda1 "))B/$(awk '/^\/dev/ {print $2}' <(df -H | grep "/dev/sda1 "))B
+  \    /\   $(tput bold)$(whoami)@$(hostname) on ${REL:13}$(tput sgr0)
+   )  ( ')  $(tput bold)CPU:$(tput sgr0) $(mpstat | awk 'END{print 100-$NF"%"}')
+  (  /  )   $(tput bold)MEM:$(tput sgr0) $(awk '/^Mem/ {print $3}' <(free -m))mb/$(awk '/^Mem/ {print $2}' <(free -m))mb
+   \(__)|   $(tput bold)DSK:$(tput sgr0) $(awk '/^\/dev/ {print $3}' <(df -H | grep "/dev/sda1 "))B/$(awk '/^\/dev/ {print $2}' <(df -H | grep "/dev/sda1 "))B
 
-  Uptime: ${UP:3}
-  Packages: $(dpkg --list | wc --lines)
+  $(tput bold)Uptime:$(tput sgr0) ${UP:3}
+  $(tput bold)Packages:$(tput sgr0) $(dpkg --list | wc --lines)
 " | lolcat
